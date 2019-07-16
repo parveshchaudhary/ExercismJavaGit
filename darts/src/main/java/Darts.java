474490057f66207c3e3242140c5a8084dc92d72f
Parvesh Chaudhary <parveshchaudhary111@gmail.com>
2019-07-16 12:45:54 +0530

    Code Addition v5

--- darts/src/main/java/Darts.java
+++ darts/src/main/java/Darts.java
@@ -1,9 +1,16 @@
 class Darts {
 
-    int score = 0;
+    double x,y;
+    int score=0;
 
     Darts(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int score() {
         double dist = Math.sqrt( (x*x) + (y*y) );
+        
         if(dist<=1){
             score = 10;
         } else if(dist<=5){
@@ -11,9 +18,6 @@ class Darts {
         } else if(dist<=10){
             score = 1;
         }
-    }
-
-    int score() {
         return score;
     }
 }
--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -3,36 +3,45 @@ import java.util.*;
 
 class Scrabble {
 
-    String word;
+  String word;
 
-    Scrabble(String word) {
-        this.word = word.toLowerCase();
+  Pattern p1 = Pattern.compile("[aeioulnrst]");
+  Pattern p2 = Pattern.compile("[dg]");
+  Pattern p3 = Pattern.compile("[bcmp]");
+  Pattern p4 = Pattern.compile("[fhvwy]");
+  Pattern p5 = Pattern.compile("[k]");
+  Pattern p6 = Pattern.compile("[jx]");
+  Pattern p7 = Pattern.compile("[qz]");
+
+  Scrabble(String word) {
+    this.word = word.toLowerCase();
+  }
+
+  int getScore() {
+    int score = 0;
+
+    for (int i = 0; i < word.length(); i++) {
+
+      String charVal = Character.toString(word.charAt(i));
+
+      if (p1.matcher(charVal).matches()) {
+        score += 1;
+      } else if (p2.matcher(charVal).matches()) {
+        score += 2;
+      } else if (p3.matcher(charVal).matches()) {
+        score += 3;
+      } else if (p4.matcher(charVal).matches()) {
+        score += 4;
+      } else if (p5.matcher(charVal).matches()) {
+        score += 5;
+      } else if (p6.matcher(charVal).matches()) {
+        score += 8;
+      } else if (p7.matcher(charVal).matches()) {
+        score += 10;
+      }
     }
 
-    int getScore() {
-        int score = 0;
-
-        for (int i = 0; i < word.length(); i++) {
-
-              if(Pattern.matches("[aeioulnrst]", Character.toString(word.charAt(i)))){
-                score += 1;
-              } else if(Pattern.matches("[dg]", Character.toString(word.charAt(i)))){
-                score += 2;
-              } else if(Pattern.matches("[bcmp]", Character.toString(word.charAt(i)))){
-                score += 3;
-              } else if(Pattern.matches("[fhvwy]", Character.toString(word.charAt(i)))){
-                score += 4;
-              } else if(Pattern.matches("[k]", Character.toString(word.charAt(i)))){
-                score += 5;
-              } else if(Pattern.matches("[jx]", Character.toString(word.charAt(i)))){
-                score += 8;
-              } else if(Pattern.matches("[qz]", Character.toString(word.charAt(i)))){
-                score += 10;
-              }
-            
-        }
-
-        return score;
-    }
+    return score;
+  }
 
 }

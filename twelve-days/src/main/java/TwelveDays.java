be89793522802ab15f29f036869d0b1fb7405cb6
Parvesh Chaudhary <parveshchaudhary111@gmail.com>
2019-07-20 22:18:02 +0530

    Code Update v2

--- twelve-days/src/main/java/TwelveDays.java
+++ twelve-days/src/main/java/TwelveDays.java
@@ -1,9 +1,9 @@
 class TwelveDays {
 
-    String days[] = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth",
+    private static final String days[] = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth",
             "eleventh", "twelfth", };
 
-    String[] items = { "twelve Drummers Drumming", "eleven Pipers Piping", "ten Lords-a-Leaping", "nine Ladies Dancing",
+    private static final String[] giftsReceived = { "twelve Drummers Drumming", "eleven Pipers Piping", "ten Lords-a-Leaping", "nine Ladies Dancing",
             "eight Maids-a-Milking", "seven Swans-a-Swimming", "six Geese-a-Laying", "five Gold Rings",
             "four Calling Birds", "three French Hens", "two Turtle Doves", "a Partridge in a Pear Tree" };
 
@@ -13,11 +13,11 @@ class TwelveDays {
 
         verse.append(String.format("On the %s day of Christmas my true love gave to me: ", days[verseNumber - 1]));
 
-        for (i = (items.length - verseNumber); i < items.length - 1; i++) {
-            verse.append(String.format("%s, ", items[i]));
+        for (i = (giftsReceived.length - verseNumber); i < giftsReceived.length - 1; i++) {
+            verse.append(String.format("%s, ", giftsReceived[i]));
         }
 
-        return String.format("%s%s%s.\n", verse.toString(), (verseNumber == 1) ? "" : "and ", items[i]);
+        return String.format("%s%s%s.\n", verse.toString(), (verseNumber == 1) ? "" : "and ", giftsReceived[i]);
     }
 
     String verses(int startVerse, int endVerse) {
